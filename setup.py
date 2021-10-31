f46376bf1edb3a3cfb382421a01b02a6ac5a20cc
Plaban1981 <nayakpplaban@gmail.com>
2021-10-31 08:58:25 +0530

    setup.py added

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "Ddvc-nlp-project-demo"
+AUTHOR_USER_NAME = "plaban1981"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -14,11 +14,11 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="A small package for DVC-NLP",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/plaban1981/dvc-nlp-project-demo",
+    author_email="nayakpplaban@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
